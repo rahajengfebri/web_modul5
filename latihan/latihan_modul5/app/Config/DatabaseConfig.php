9fd940fd2a5f0353a11ba9b1e125df4201e44da1
rahajengfebri <127112437+rahajengfebri@users.noreply.github.com>
2023-11-27 21:04:05 +0700

    Add files via upload

---
+++ latihan/latihan_modul5/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\config;
+
+class DatabaseConfig {
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "praktikum";
+    public $port = 3306;
+}
